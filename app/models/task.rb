c1c1adbba3bd60afdae8d819b041045fed066f99
snowmint <starlitsky0612@gmail.com>
2021-06-22 16:07:41 +0800

    [Travis Update / Add Rspec] testing update

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,9 +5,18 @@ class Task < ApplicationRecord
   validates_presence_of :start_time
   validates_presence_of :end_time
   validates_presence_of :task_name
-  validates_presence_of :content
   validates_presence_of :priority
   validates_presence_of :status
   validates_numericality_of :status, :less_than_or_equal_to => 10
   validates_numericality_of :priority, :less_than_or_equal_to => 6
+  
+  validate :validate_content_with_task_name
+  def validate_content_with_task_name
+    if ( task_name.present? && content.blank? )
+      errors.add(:content, "Must have content with task title.")
+    end
+    if ( task_name.blank? && content.present? )
+      errors.add(:task_name, "Must have task title with content.")
+    end
+  end
 end
