ac5701a74f90eed35315437e050846a0a116b06b
snowmint <starlitsky0612@gmail.com>
2021-06-22 23:36:26 +0800

    [Step 12] update validation and error message internationalization

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,22 +1,19 @@
 class Task < ApplicationRecord
+  #relation
   belongs_to :user
   has_many :label_map
-  validates_presence_of :user_id
-  validates_presence_of :start_time
-  validates_presence_of :end_time
-  validates_presence_of :task_name
-  validates_presence_of :priority
-  validates_presence_of :status
-  validates_numericality_of :status, :less_than_or_equal_to => 10
-  validates_numericality_of :priority, :less_than_or_equal_to => 6
-  
+  #validates
+  validates :user_id, :start_time, :end_time, :task_name, presence: true
+  validates :priority, presence: true, numericality: {less_than_or_equal_to: 10}
+  validates :status, presence: true, numericality: {less_than_or_equal_to: 6}
   validate :validate_content_with_task_name
+  #method
   def validate_content_with_task_name
     if ( task_name.present? && content.blank? )
-      errors.add(:content, "Must have content with task title.")
+      errors.add(:content, I18n.t('errors.task_name_without_content'))
     end
     if ( task_name.blank? && content.present? )
-      errors.add(:task_name, "Must have task title with content.")
+      errors.add(:task_name, I18n.t('errors.content_without_task_name'))
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,22 @@
 class User < ApplicationRecord
+  #relation
+  has_many :task
+  #validates
+  validates :username, :presence => { :message => I18n.t('errors.username_blank') },
+                       :uniqueness => { :message => I18n.t('errors.username_uniqueness') }
+  validates_presence_of :password
+  validate :password_requirements
+  #constant
+  Rules = { #constant: start with capital letters
+    I18n.t('errors.password_no_lowercase')  => /[a-z]+/,
+    I18n.t('errors.password_no_uppercase')  => /[A-Z]+/,
+    I18n.t('errors.password_no_digit')      => /\d+/
+  }
+  #method
   def password_requirements
-    rules = {
-      "must contain at least one lowercase letter"  => /[a-z]+/,
-      "must contain at least one uppercase letter"  => /[A-Z]+/,
-      "must contain at least one digit"             => /\d+/
-    }
-  
-    rules.each do |message, regex|
+    Rules.each do |message, regex|
       errors.add( :password, message ) unless password.match( regex )
     end
   end
-  
-  has_many :task
-  validates :name, :presence => {:message => "Can't be blank"},
-                   :uniqueness => {:message => "Error: Duplicate Username"}
-  validates_presence_of :password
-  validate :password_requirements
-
   #validates_format_of :password :with => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/', message: 'Need atleast one '   
 end
