076905d622bd6699597ce77589a066ecdf90e3e1
snowmint <starlitsky0612@gmail.com>
2021-07-09 00:41:33 +0800

    [Modify] remove repeat code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = I18n.t('user_relate.add_success')
       if current_user.admin?
-        flash[:success] = I18n.t('user_relate.add_success')
         redirect_to list_user_path
       else
-        flash[:success] = I18n.t('user_relate.add_success')
         redirect_to login_path
       end
     else
@@ -50,7 +49,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      custom_params = params.require(:user).permit(:username, :email, :password, :password_confirmation)
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 
     def logged_in_user
