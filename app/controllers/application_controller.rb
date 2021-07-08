163e9612504703e36a107f8b2f0f33d38a96f5ef
snowmint <starlitsky0612@gmail.com>
2021-07-08 19:27:31 +0800

    [Modify] split cmds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,16 @@ class ApplicationController < ActionController::Base
     session[:locale] = params[:locale] if params[:locale] && I18n.available_locales.include?( params[:locale].to_s.to_sym )
     I18n.locale = session[:locale] || I18n.default_locale
   end
+
+  def split_order_cmds
+    @split_order_cmds ||= params[:order].to_s.split
+  end
   
   def sort_column
-    split_order = params[:order].to_s.split()
-    Task.column_names.include?(split_order[0]) ? split_order[0] : "id"
+    Task.column_names.include?(split_order_cmds[0]) ? split_order_cmds[0] : "id"
   end
   
   def sort_direction
-    split_order = params[:order].to_s.split()
-    %w[asc desc].include?(split_order[1]) ? split_order[1] : "asc"
+    %w[asc desc].include?(split_order_cmds[1]) ? split_order_cmds[1] : "asc"
   end
 end
