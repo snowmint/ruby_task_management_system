debd54dc424b778f80ad0d0fbbed7b5a7de22ced
snowmint <starlitsky0612@gmail.com>
2021-07-23 14:14:03 +0800

    [Modify] Remove raw() turn into I18n.t()

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -46,7 +46,6 @@ class Admin::UsersController < Admin::BaseController
   end
 
   private
-  
 
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation, :admin)
